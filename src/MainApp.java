562342f3e98f1239079bdaf02da79aa71d9fdc83
Juanda <Juanda@Colossus>
2017-05-07 12:05:11 -0500

    Main APP

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,23 +1,35 @@
 import processing.core.PApplet;
 
-public class MainApp extends PApplet  {
+public class MainApp extends PApplet {
 
 	public static void main(String[] args) {
 		PApplet.main("MainApp");
-	
+
 	}
-	
-	
+
+	Mundo m;
+
 	@Override
 	public void settings() {
 		size(1200, 700);
 	}
-	
+
 	@Override
 	public void setup() {
-		// TODO Auto-generated method stub
-		super.setup();
+
+		m = new Mundo();
 	}
-	
+
+	@Override
+	public void draw() {
+		background(255);
+		m.pintar(this);
+
+	}
+
+	@Override
+	public void mousePressed() {
+		m.crearAlimento(mouseX, mouseY);
+	}
 
 }
---
+++ src/Mundo.java
@@ -0,0 +1,14 @@
+
+public class Mundo {
+
+	public void pintar(MainApp mainApp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void crearAlimento(int mouseX, int mouseY) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
