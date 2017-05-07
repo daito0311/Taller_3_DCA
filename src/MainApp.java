0623f3ce16151bd51dc144516e5561ec2c92da3a
Juanda <Juanda@Colossus>
2017-05-07 11:47:27 -0500

    base

---
+++ src/MainApp.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class MainApp extends PApplet  {
+
+	public static void main(String[] args) {
+		PApplet.main("MainApp");
+	
+	}
+	
+	
+	@Override
+	public void settings() {
+		size(1200, 700);
+	}
+	
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+	
+
+}
